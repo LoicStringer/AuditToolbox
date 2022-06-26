c8ef250c0c014d137179dca8f3853dae18e88855
Loïc Stringer <loic.stringer@gmail.com>
2022-06-26 17:08:31 +0200

    Initial Commit

---
+++ src/main/java/com/audit/toolbox/AuditToolboxApplication.java
@@ -0,0 +1,13 @@
+package com.audit.toolbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuditToolboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuditToolboxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/audit/toolbox/AuditToolboxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.audit.toolbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuditToolboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
